4ac7c9490d120e06c104c1402573144ea9d371a1
Kentaro Ema <ema.s.kitchen0@gmail.com>
2024-10-09 18:33:07 +0900

    Add base64 to reqirements

--- app.py
+++ app.py
@@ -557,6 +557,35 @@ if st.button("スピナーを表⽰"):
 # st.write("これはラテックスのテキストです。")
 # st.write("これはボックスのテキストです。")
 
+import streamlit as st
+import base64
+import os
+
+def upload_pdf_file():
+    uploaded_file = st.file_uploader("Upload PDF file", type="pdf")
+    if uploaded_file is None:
+        st.write("Please upload a PDF file.")
+    return uploaded_file
+
+def display_pdf(uploaded_file):
+    if uploaded_file is not None:
+        pdf_contents = uploaded_file.read()
+        pdf_base64 = base64.b64encode(pdf_contents).decode('utf-8')
+        encoded_pdf = f'<embed src="data:application/pdf;base64,{pdf_base64}" width="800" height="600" type="application/pdf">'
+        st.markdown(encoded_pdf, unsafe_allow_html=True)
+
+    else:
+        st.write("No PDF file uploaded.")
+        st.write(pdf_contents, unsafe_allow_html=True)
+
+def main():
+    file = upload_pdf_file()
+
+    if file is not None:
+        display_pdf(file)
+
+main()
+
 
 
 
